a14140cc777a4c8450de7a371710b92741342d4e
charan1435 <sc16101@gmail.com>
2024-11-24 15:48:02 +0530

    Initial Commit

---
+++ ModelViewController/ModelViewControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelViewControllerApp.swift
+//  ModelViewController
+//
+//  Created by Sabith Salman on 2024-11-24.
+//
+
+import SwiftUI
+
+@main
+struct ModelViewControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ModelViewControllerTests/ModelViewControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ModelViewControllerTests.swift
+//  ModelViewControllerTests
+//
+//  Created by Sabith Salman on 2024-11-24.
+//
+
+import Testing
+@testable import ModelViewController
+
+struct ModelViewControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
